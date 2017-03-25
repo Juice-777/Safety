8a654f68fc48df871c60c76e04b0f2164bc1a667
Ilia Shubin <ioshubin@yandex.ru>
2017-03-25 14:25:47 +0200

    Add project files.

---
+++ Safety/Models/AppDbInitializer.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
+
+namespace Safety.Models
+{
+    public class AppDbInitializer : DropCreateDatabaseAlways<ApplicationDbContext>
+    {
+        protected override void Seed(ApplicationDbContext context)
+        {
+            var userManager = new ApplicationUserManager(new UserStore<ApplicationUser>(context));
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+
+            // создаем две роли
+            var role1 = new IdentityRole { Name = "manager" };
+
+            // добавляем роли в бд
+            roleManager.Create(role1);
+
+            // создаем пользователей
+            var admin = new ApplicationUser { Email = "somemail11@mail.ru", UserName = "somemail11@mail.ru" };
+            string password = "ad46D_ewr3";
+            var result = userManager.Create(admin, password);
+
+            // если создание пользователя прошло успешно
+            if (result.Succeeded)
+            {
+                // добавляем для пользователя роль
+                userManager.AddToRole(admin.Id, role1.Name);
+            }
+
+            base.Seed(context);
+        }
+    }
+}
---
+++ Safety/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Safety.Startup))]
+namespace Safety
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
